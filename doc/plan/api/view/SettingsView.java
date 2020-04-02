0d2c1fcb0070f82736aa2348f6587ce3bb1ad9c9
Selena Qian <sq22@duke.edu>
2020-04-02 00:56:29 -0400

    copied view interfaces so far over to the doc folder

---
+++ doc/plan/api/view/SettingsView.java
@@ -0,0 +1,15 @@
+package ooga.view;
+
+/**
+ * A SettingsView handles the user interface for settings including saving and resuming a game, starting the game over without
+ * saving, dark mode, changing one particular color or image.
+ */
+public interface SettingsView {
+    //TODO: so for listeners does there need to be a getter method for the button?
+
+    /**
+     * Gets information from a text field when saving a configuration for later.
+     * @return the desired filename for the configuration.
+     */
+    public String getSaveFileName();
+}

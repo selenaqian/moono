1fd69beb7e97a1097f3f611f62833a0f2e99824e
Selena Qian <sq22@duke.edu>
2020-04-02 16:32:06 -0400

    created third use case

---
+++ doc/plan/usecases/UseCase3.java
@@ -0,0 +1,11 @@
+/**
+ * This class demonstrates the use case:
+ * Choose card design theme - e.g. Duke theme, space theme
+ *     - create view of cards with the particular theme
+ * This use case focuses on the frontend, allowing users to click on a node or button that would then spur changes in the
+ * visual output of the cards.
+ */
+
+public class UseCase3 {
+    // The following code would appear within a method called when the node is clicked
+}

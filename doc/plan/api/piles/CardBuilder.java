f9728a3c489682901c7961e6033b6f4c5b48182d
Tess Noonan <tcn6@duke.edu>
2020-04-02 17:55:26 -0600

    added custom combo method in CardBuilder

--- doc/plan/api/piles/CardBuilder.java
+++ doc/plan/api/piles/CardBuilder.java
@@ -8,8 +8,17 @@ import java.util.List;
 public interface CardBuilder {
 
     /**
-     * This method instantiates every possible Card combination and returns them as a List.
+     * This method instantiates 2 of every possible Card combination and returns them as a Stack.
+     * This is the default option.
      * @return full deck of cards
      */
-    List<Card> makeFullDeck();
+    Stack<Card> makeDeck();
+
+    /**
+     * This method creates a Deck using Cards with the given Actions.
+     * There will be 8 of every Card with Action.NONE (2 per suit)
+     * There will be 4 of every Card without Action.NONE (1 per suit)
+     * @return custom deck of cards
+     */
+    Stack<Card> makeDeck(List<Action> actions);
 }
--- src/ooga/piles/CardBuilder.java
+++ src/ooga/piles/CardBuilder.java
@@ -1,8 +1,10 @@
 package ooga.piles;
 
+import ooga.cards.Action;
 import ooga.cards.Card;
 
 import java.util.List;
+import java.util.Stack;
 
 /**
  * This class instantiates Cards and compiles them into a List.
@@ -14,4 +16,12 @@ public interface CardBuilder {
      * @return full deck of cards
      */
     List<Card> makeFullDeck();
+
+    /**
+     * This method creates a Deck using Cards with the given Actions.
+     * There will be 8 of every Card with Action.NONE (2 per suit)
+     * There will be 4 of every Card without Action.NONE (1 per suit)
+     * @return custom deck of cards
+     */
+    Stack<Card> makeDeck(List<Action> actions);
 }

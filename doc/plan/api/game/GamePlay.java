2ec14d59d0355eae5c2a1789f99660a282a1c282
Mary Jiang <mvj6@duke.edu>
2020-04-04 16:27:07 -0600

    made changes to APIs! Updated GamePlay interface to include additional methods to communicate with view based on TA meeting feedback

--- doc/plan/api/game/GamePlay.java
+++ doc/plan/api/game/GamePlay.java
@@ -15,15 +15,30 @@ public interface GamePlay{
 
     // The following two methods are specific to uno and may be left out of the interface
     // They're helpful to teammates! :)
+
     /**
-     * Once a player has no cards left, check if they have declared uno
-     * If a player has not declared, then they must pick up more cards
+     * Handle behavior when a user selects a card to play it
+     * Called from the view
      */
-    void checkUno();
+    void makePlay();
+
+    /**
+     * When a user isn't able to play a card and they take a card from the draw pile
+     * Called from the view
+     */
+    void drawCard();
 
     /**
      * Allows the human player to declare uno
      * Called from view class when the "uno" button is clicked
      */
     void callUno();
+
+    /**
+     * Once a player has no cards left, check if they have declared uno
+     * If a player has not declared, then they must pick up more cards
+     */
+    void checkUno();
+
+
 }

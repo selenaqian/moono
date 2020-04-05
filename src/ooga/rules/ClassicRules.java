b61b19ff236c831b5a9564d501d564a3b3d4bd96
Tess Noonan <tcn6@duke.edu>
2020-04-05 12:45:52 -0600

    wrote ClassicRules

---
+++ src/ooga/rules/ClassicRules.java
@@ -0,0 +1,34 @@
+package ooga.rules;
+
+import ooga.cards.Card;
+import ooga.piles.Hand;
+
+/**
+ * This class follows the classic uno rules: (WHICH IS ONLY USING BASIC CARDS FOR SPRINT 1)
+ *      Must play a card that is either of same Suit or Value as the Card on top of DiscardPile.
+ *      Game is over when the last card in a player's hand is valid.
+ */
+public class ClassicRules implements Rule {
+
+    /**
+     * Create new ClassicRules object.
+     */
+    public ClassicRules() {
+    }
+
+    @Override
+    public boolean isValid(Card discard, Card playedCard) {
+        if(playedCard.getSuit() == discard.getSuit() || playedCard.getValue() == discard.getValue()){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isOver(Card discard, Hand hand) {
+        if(hand.getCardCount() == 1 && isValid(discard, hand.getAllCards().get(0))){
+            return true;
+        }
+        return false;
+    }
+}

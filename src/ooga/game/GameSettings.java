c42241551cf2a3d62e6d6b9267ee072c0d1824c0
Mary Jiang <mvj6@duke.edu>
2020-04-06 15:48:51 -0600

    added GameSettings

---
+++ src/ooga/game/GameSettings.java
@@ -0,0 +1,43 @@
+package ooga.game;
+
+/**
+ *
+ */
+public class GameSettings {
+
+    private int numPlayers; //set to default 4 players
+    private int handSize; //number of cards per player, set to default 7
+    private int winningScore; //default 500 for uno
+
+    public GameSettings(){
+        numPlayers = 4;
+        handSize = 7;
+        winningScore = 500;
+    }
+
+    public int getNumPlayers(){
+        return numPlayers;
+    }
+
+    public int getHandSize(){
+        return handSize;
+    }
+
+    public int getWinningScore(){
+        return winningScore;
+    }
+
+    public void setNumPlayers(int numPlayers){
+        this.numPlayers = numPlayers;
+    }
+
+    public void setHandSize(int handSize){
+        this.handSize = handSize;
+    }
+
+    public void setWinningScore(int winningScore){
+        this.winningScore = winningScore;
+    }
+
+    //TODO: add setters and getters for: Rules, Themes, Special cards
+}

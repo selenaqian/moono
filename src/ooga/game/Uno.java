a480f5ac88d35a8b7f985386da1c9ccc89d2bdb8
Mary Jiang <mvj6@duke.edu>
2020-04-06 15:39:15 -0600

    handling player plays without pilemanager for now

--- src/ooga/game/Uno.java
+++ src/ooga/game/Uno.java
@@ -1,8 +1,11 @@
 package ooga.game;
 
 import ooga.cards.Card;
+import ooga.piles.DiscardPile;
+import ooga.piles.DrawPile;
 import ooga.piles.Hand;
 import ooga.player.Player;
+import ooga.rules.Rule;
 
 /**
  * Controller class for handling play of Uno
@@ -13,26 +16,44 @@ public class Uno {
     private View view;
 
     private UnoTurnManager turnManager;
-    private PileManager pileManager;
     private Player currentPlayer;
 
+    private DiscardPile discPile;
+    private DrawPile drawPile;
+
+    private Rule rule;
 
     public Uno(){
         //initialize connection to view
         this.view = new View();
         turnManager = new UnoTurnManager();
-        pileManager = new PileManager();
         currentPlayer = turnManager.getCurrentPlayer();
+
+        discPile = new DiscardPile();
+        drawPile = new DrawPile();
+    }
+
+    public void setRules(Rule rule){
+        this.rule = rule;
     }
 
     /**
      * Handle behavior when a user selects a card to play it
      * Called from the view
-     * @param card the card selected by the player in the view
+     * @param selectedCard the card selected by the player in the view or selected by the AI player
      */
-    public void playCard(Card card){
-        //get the player to play a valid card from their hand
-        currentPlayer.playCard(card);
+    public void playCard(Card selectedCard){
+        //check if played card can be played on top of the discard pile top card
+        if (rule.isValid(discPile.showTopCard(), selectedCard)) {
+
+            //make sure player updates their hand to remove the card
+            currentPlayer.playCard(selectedCard);
+
+            //update the discard pile to add the card
+            discPile.addCard(selectedCard); //TODO: make sure addCard adds to the top of the pile?
+        }
+
+        //TODO: make call to handleAction() once special cards are implemented
 
         //end current player's turn and go to next player
         turnManager.nextPlayer();
@@ -44,8 +65,8 @@ public class Uno {
      * For AI players, must be called programmatically
      */
     public void drawCard(){
-        //take the top card from the discard pile
-        Card card = pileManager.removeCard();
+        //take the top card from the discard pile, make sure it is removed from the discard pile
+        Card card = discPile.popTopCard();
 
         //get player to accept the drawn card into their own hand of cards
         currentPlayer.takeCard(card);
@@ -55,7 +76,7 @@ public class Uno {
     }
 
     /**
-     * Handle effect of an action card when it is drawn
+     * Handle effect of an action card when it played
      * This method should have access to TurnManager, especially for Reverse cards
      * TODO: put action methods in a resource file and use this method to call them
      */

207772469328797a5813d8985c6403721188a36e
Tess Noonan <tcn6@duke.edu>
2020-04-07 13:23:28 -0600

    commented CardTest, started DeckTest

---
+++ test/ooga/piles/DeckTest.java
@@ -0,0 +1,34 @@
+package ooga.piles;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+    @BeforeEach
+    void setUp() {
+        Deck deck1 = new DiscardPile();
+    }
+
+    @Test
+    void shuffle() {
+    }
+
+    @Test
+    void addCard() {
+    }
+
+    @Test
+    void addCards() {
+    }
+
+    @Test
+    void getAllCards() {
+    }
+
+    @Test
+    void getCardCount() {
+    }
+}

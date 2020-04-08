5d197cfa8cb4b7d5413863659509e083646f99fd
Tess Noonan <tcn6@duke.edu>
2020-04-08 16:13:12 -0600

    finished shuffle and addCard tests

--- test/ooga/piles/DeckTest.java
+++ test/ooga/piles/DeckTest.java
@@ -1,9 +1,10 @@
 package ooga.piles;
 
-import ooga.cards.Card;
+import ooga.cards.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,7 +15,8 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class DeckTest {
 
-    private Deck deck1 = new DrawPile();
+    //TODO: Get rid of the instance var/before each
+    private Deck deck1;
 
     /**
      * Start each test with a fresh Deck.
@@ -32,20 +34,69 @@ class DeckTest {
     @Test
     void shuffle() {
         Stack<Card> deck1Start = deck1.getAllCards();
+        assertEquals(deck1Start, deck1.getAllCards());
         deck1.shuffle();
         assertNotEquals(deck1Start, deck1.getAllCards());
         Stack<Card> deck1Next = deck1.getAllCards();
+        assertEquals(deck1Next, deck1.getAllCards());
         deck1.shuffle();
         assertNotEquals(deck1Next, deck1.getAllCards());
     }
 
+    /**
+     * Tests the shuffling of an empty Deck to show that the program does not break
+     * and that the Stacks are equal as expected (both empty).
+     */
     @Test
     void shuffleEmptyDeck() {
-
+        Deck deck2 = new DiscardPile();
+        Stack<Card> deck2Start = deck2.getAllCards();
+        assertEquals(deck2Start, deck2.getAllCards());
+        deck2.shuffle();
+        assertEquals(deck2Start, deck2.getAllCards());
     }
 
+    /**
+     * Tests the shuffling of a Deck with one Card to show that the program does not break
+     * and that the Stacks are equal as expected (both have the same single Card).
+     */
+    @Test
+    void shuffleDeckOfOne() {
+        Deck deck3 = new DiscardPile(new Card(Suit.C, Value.SEVEN));
+        Stack<Card> deck2Start = deck3.getAllCards();
+        assertEquals(deck2Start, deck3.getAllCards());
+        deck3.shuffle();
+        assertEquals(deck2Start, deck3.getAllCards());
+    }
+
+    /**
+     * Tests that addCard() adds a single Card to the top of the Deck.
+     */
     @Test
     void addCard() {
+        Card card1 = new Card(Suit.D, Value.NINE);
+        Card card2 = new Card(Suit.B, Value.SIX);
+        Deck deck4 = new DiscardPile(card1);
+        assertEquals(card1, deck4.getAllCards().peek());
+
+        deck4.addCard(card2);
+        assertEquals(card2, deck4.getAllCards().pop());
+        assertEquals(card1, deck4.getAllCards().pop());
+    }
+
+    /**
+     * Tests that addCard() adds a single Card to the top of the Deck
+     * despite it being a duplicate to one already present.
+     */
+    @Test
+    void addDuplicateCard() {
+        Card card1 = new Card(Suit.D, Value.NINE);
+        Deck deck5 = new DiscardPile(card1);
+        assertEquals(card1, deck5.getAllCards().peek());
+
+        deck5.addCard(card1);
+        assertEquals(card1, deck5.getAllCards().pop());
+        assertEquals(card1, deck5.getAllCards().pop());
     }
 
     @Test

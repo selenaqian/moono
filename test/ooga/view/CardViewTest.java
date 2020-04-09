4519968311aff79b0a71af3a684f3f4ce5fb9de4
Selena Qian <sq22@duke.edu>
2020-04-09 11:54:17 -0400

    wrote two more tests for CardView

--- test/ooga/view/CardViewTest.java
+++ test/ooga/view/CardViewTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CardViewTest {
     @Test
-    void createCardSimpleTest() {
+    void createCardSimpleTest1() {
         Card card1 = new Card(Suit.A, Value.ZERO);
 
         CardView card1View = new CardView(card1, 100, 200);
@@ -21,5 +21,33 @@ class CardViewTest {
         assertEquals(Color.RED, card1View.getCardViewBase().getFill());
         assertEquals("0", card1View.getCardViewText().getText());
         assertEquals(Color.WHITE, card1View.getCardViewText().getFill());
+    }
+
+    @Test
+    void createCardSimpleTest2() {
+        Card card2 = new Card(Suit.B, Value.TWO);
+
+        CardView card2View = new CardView(card2, 100, 200);
+
+        assertEquals(0, card2.compareTo(card2View.getCard()));
+        assertEquals(100, card2View.getCardViewBase().getWidth());
+        assertEquals(200, card2View.getCardViewBase().getHeight());
+        assertEquals(Color.BLUE, card2View.getCardViewBase().getFill());
+        assertEquals("2", card2View.getCardViewText().getText());
+        assertEquals(Color.WHITE, card2View.getCardViewText().getFill());
+    }
+
+    @Test
+    void createCardSimpleTest3() {
+        Card card3 = new Card(Suit.D, Value.FOUR);
+
+        CardView card3View = new CardView(card3, 100, 200);
+
+        assertEquals(0, card3.compareTo(card3View.getCard()));
+        assertEquals(100, card3View.getCardViewBase().getWidth());
+        assertEquals(200, card3View.getCardViewBase().getHeight());
+        assertEquals(Color.YELLOW, card3View.getCardViewBase().getFill());
+        assertEquals("4", card3View.getCardViewText().getText());
+        assertEquals(Color.BLACK, card3View.getCardViewText().getFill());
     }
 }

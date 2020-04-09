ef35948d3a86538ca446616ca58b08a6a1cac69e
Tess Noonan <tcn6@duke.edu>
2020-04-09 15:51:46 -0600

    created classicrulestest class skeleton

---
+++ test/ooga/rules/ClassicRulesTest.java
@@ -0,0 +1,25 @@
+package ooga.rules;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This tests the ClassicRules class.
+ * @author Tess Noonan (tcn6)
+ */
+class ClassicRulesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void isValid() {
+    }
+
+    @Test
+    void isOver() {
+    }
+}

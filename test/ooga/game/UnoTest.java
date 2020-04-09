437af83513bcf1c919d9601d56d29c8efdca31e6
Mary Jiang <mvj6@duke.edu>
2020-04-09 13:48:02 -0600

    added test class for Uno

---
+++ test/ooga/game/UnoTest.java
@@ -0,0 +1,22 @@
+package ooga.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnoTest {
+
+
+
+    @Test
+    void playCard() {
+    }
+
+    @Test
+    void drawCard() {
+    }
+
+    @Test
+    void getUserHand() {
+    }
+}

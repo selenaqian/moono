bce49b66c2fee626f38b31e4f973973feb16f47c
Selena Qian <sq22@duke.edu>
2020-04-09 03:33:19 -0400

    wrote tests for updating cards in player hand

--- test/ooga/view/GameViewTest.java
+++ test/ooga/view/GameViewTest.java
@@ -1,5 +1,9 @@
 package ooga.view;
 
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import ooga.cards.Card;
@@ -43,8 +47,48 @@ class GameViewTest extends DukeApplicationTest {
      * Tests updating the visual of player 1's hand where cards are actually visible.
      */
     @Test
-    void testUpdateHand() {
+    void testUpdateHandOneNew() {
+        sleep(5, TimeUnit.SECONDS);
+        List<Card> newCards = new ArrayList<>();
+        newCards.add(new Card(Suit.B, Value.FIVE));
+        javafxRun(() -> gameView.updateHand(newCards));
+        sleep(5, TimeUnit.SECONDS);
 
+        HBox hand = gameView.getPlayer1Hand();
+        List<CardView> cards = new ArrayList<>();
+        for(int i=0; i< hand.getChildren().size(); i++) {
+            cards.add((CardView)hand.getChildren().get(i));
+        }
+
+        assertEquals(0, new Card(Suit.B, Value.FIVE).compareTo(cards.get(0).getCard()));
+        assertTrue(new Rectangle(100, 600/4, Color.BLUE).equals(cards.get(0).getChildren().get(0)));
+        assertEquals("5", cards.get(0).getChildren().get(1).toString());
+    }
+
+    @Test
+    void testUpdateHandMultipleNew() {
+        sleep(5, TimeUnit.SECONDS);
+        List<Card> newCards = new ArrayList<>();
+        newCards.add(new Card(Suit.B, Value.FIVE));
+        for(int i=1; i<6; i++) newCards.add(new Card(Suit.C, Value.NINE));
+        javafxRun(() -> gameView.updateHand(newCards));
+        sleep(5, TimeUnit.SECONDS);
+
+        HBox hand = gameView.getPlayer1Hand();
+        List<CardView> cards = new ArrayList<>();
+        for(int i=0; i< hand.getChildren().size(); i++) {
+            cards.add((CardView)hand.getChildren().get(i));
+        }
+
+        assertEquals(0, new Card(Suit.B, Value.FIVE).compareTo(cards.get(0).getCard()));
+        assertTrue(new Rectangle(1000/6-5, 600/4, Color.BLUE).equals(cards.get(0).getChildren().get(0)));
+        assertEquals("5", cards.get(0).getChildren().get(1).toString());
+
+        for(int i=0; i< hand.getChildren().size(); i++) {
+            assertEquals(0, new Card(Suit.C, Value.NINE).compareTo(cards.get(i).getCard()));
+            assertTrue(new Rectangle(1000/6-5, 600/4, Color.GREEN).equals(cards.get(i).getChildren().get(0)));
+            assertEquals("9", cards.get(i).getChildren().get(1).toString());
+        }
     }
 
     @Test

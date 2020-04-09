30c864baa80d44180c87e495abf400c0f51f545a
Tess Noonan <tcn6@duke.edu>
2020-04-08 20:46:52 -0600

    finished Deck tests

--- src/ooga/game/Uno.java
+++ src/ooga/game/Uno.java
@@ -1,155 +1,155 @@
-package ooga.game;
-
-import ooga.cards.Card;
-import ooga.piles.DiscardPile;
-import ooga.piles.DrawPile;
-import ooga.player.Player;
-import ooga.rules.Rule;
-import ooga.view.GameView;
-import ooga.view.GameViewInterface;
-
-import java.util.List;
-
-/**
- * Model class for handling play of Uno
- * Equivalent to GamePlay interface from planning
- */
-public class Uno implements GameModel {
-
-    private GameViewInterface view;
-
-    private GameSettings settings;
-    private UnoController unoController;
-    private UnoTurnManager turnManager;
-    private Player currentPlayer;
-    private Player user; //the human player
-
-    private DiscardPile discPile;
-    private DrawPile drawPile;
-
-    private Rule rule;
-
-    public Uno(){
-        this(new GameSettings());
-    }
-
-    public Uno(GameSettings settings){
-        this.view = new GameView(); //TODO: change to interface
-        this.settings = settings;
-        turnManager = new UnoTurnManager();
-        unoController = new UnoController();
-        addPlayers();
-    }
-
-    @Override
-    public void start() {
-        discPile = new DiscardPile();
-        drawPile = new DrawPile();
-        currentPlayer = turnManager.getCurrentPlayer();
-        user = currentPlayer; //TODO: change this so that the human doesn't always start first
-        dealCards();
-    }
-
-    /**
-     * Handle behavior when a user selects a card to play it
-     * Called from the view
-     * @param selectedCard the card selected by the player in the view or selected by the AI player
-     */
-    @Override
-    public void playCard(Card selectedCard){
-        //check if played card can be played on top of the discard pile top card
-        if (rule.isValid(discPile.showTopCard(), selectedCard)) {
-
-            //make sure player updates their hand to remove the card
-            //currentPlayer.playCard(selectedCard);
-
-            //update the discard pile to add the card
-            discPile.addCard(selectedCard);
-
-            //TODO: make call to handleAction() once special cards are implemented
-
-            //go to next player only when a valid card is played
-            endTurn();
-        }
-    }
-
-    /**
-     * When a user isn't able to play a card and they take a card from the draw pile
-     * Called from the view for human players
-     * For AI players, must be called programmatically
-     */
-    @Override
-    public void drawCard(){
-        //take the top card from the discard pile, make sure it is removed from the discard pile
-        Card card = drawPile.drawCard();
-
-        //get player to accept the drawn card into their own hand of cards
-        currentPlayer.takeCard(card);
-
-        endTurn();
-    }
-
-    @Override
-    public List<Card> getUserHand() {
-        return user.hand();
-    }
-
-    private void endTurn(){
-        //check if a player has no more cards
-        if (currentPlayer.hand().size() == 0){
-            unoController.endGame();
-        } else {
-            turnManager.nextPlayer();
-        }
-    }
-
-    /**
-     * Deal cards to all players in the beginning of a game
-     */
-    private void dealCards(){
-        for(int i = 0; i < settings.getNumPlayers(); i ++){
-            Player player = turnManager.getFirstPlayer();
-            for (int j = 0; j < settings.getHandSize(); j++){
-                Card card = drawPile.drawCard();
-                player.takeCard(card);
-                turnManager.nextPlayer();
-            }
-        }
-    }
-
-    /**
-     * Adds players to the game based on number of players selected in SetupView
-     * TODO: Refactor this - have another class to manage/initialize players?
-     */
-    private void addPlayers(){
-        for (int i = 0; i < settings.getNumPlayers(); i++){
-            turnManager.addPlayer(new Player());
-        }
-    }
-
-//    /**
-//     * Handle effect of an action card when it played
-//     * This method should have access to TurnManager, especially for Reverse cards
-//     * TODO: put action methods in a resource file and use this method to call them
-//     */
-//    public void handleAction(){
+//package ooga.game;
 //
+//import ooga.cards.Card;
+//import ooga.piles.DiscardPile;
+//import ooga.piles.DrawPile;
+//import ooga.player.Player;
+//import ooga.rules.Rule;
+//import ooga.view.GameView;
+//import ooga.view.GameViewInterface;
+//
+//import java.util.List;
+//
+///**
+// * Model class for handling play of Uno
+// * Equivalent to GamePlay interface from planning
+// */
+//public class Uno implements GameModel {
+//
+//    private GameViewInterface view;
+//
+//    private GameSettings settings;
+//    private UnoController unoController;
+//    private UnoTurnManager turnManager;
+//    private Player currentPlayer;
+//    private Player user; //the human player
+//
+//    private DiscardPile discPile;
+//    private DrawPile drawPile;
+//
+//    private Rule rule;
+//
+//    public Uno(){
+//        this(new GameSettings());
+//    }
+//
+//    public Uno(GameSettings settings){
+//        this.view = new GameView(); //TODO: change to interface
+//        this.settings = settings;
+//        turnManager = new UnoTurnManager();
+//        unoController = new UnoController();
+//        addPlayers();
+//    }
+//
+//    @Override
+//    public void start() {
+//        discPile = new DiscardPile();
+//        drawPile = new DrawPile();
+//        currentPlayer = turnManager.getCurrentPlayer();
+//        user = currentPlayer; //TODO: change this so that the human doesn't always start first
+//        dealCards();
 //    }
 //
 //    /**
-//     * Allows the human player to declare uno
-//     * Called from view class when the "uno" button is clicked
+//     * Handle behavior when a user selects a card to play it
+//     * Called from the view
+//     * @param selectedCard the card selected by the player in the view or selected by the AI player
 //     */
-//    public void callUno(){
+//    @Override
+//    public void playCard(Card selectedCard){
+//        //check if played card can be played on top of the discard pile top card
+//        if (rule.isValid(discPile.showTopCard(), selectedCard)) {
 //
+//            //make sure player updates their hand to remove the card
+//            //currentPlayer.playCard(selectedCard);
+//
+//            //update the discard pile to add the card
+//            discPile.addCard(selectedCard);
+//
+//            //TODO: make call to handleAction() once special cards are implemented
+//
+//            //go to next player only when a valid card is played
+//            endTurn();
+//        }
 //    }
 //
 //    /**
-//     * Once a player has no cards left, check if they have declared uno
-//     * If a player has not declared, then they must pick up more cards
+//     * When a user isn't able to play a card and they take a card from the draw pile
+//     * Called from the view for human players
+//     * For AI players, must be called programmatically
 //     */
-//    public void checkUno(){
+//    @Override
+//    public void drawCard(){
+//        //take the top card from the discard pile, make sure it is removed from the discard pile
+//        Card card = drawPile.drawCard();
 //
+//        //get player to accept the drawn card into their own hand of cards
+//        currentPlayer.takeCard(card);
+//
+//        endTurn();
 //    }
-
-}
+//
+//    @Override
+//    public List<Card> getUserHand() {
+//        return user.hand();
+//    }
+//
+//    private void endTurn(){
+//        //check if a player has no more cards
+//        if (currentPlayer.hand().size() == 0){
+//            unoController.endGame();
+//        } else {
+//            turnManager.nextPlayer();
+//        }
+//    }
+//
+//    /**
+//     * Deal cards to all players in the beginning of a game
+//     */
+//    private void dealCards(){
+//        for(int i = 0; i < settings.getNumPlayers(); i ++){
+//            Player player = turnManager.getFirstPlayer();
+//            for (int j = 0; j < settings.getHandSize(); j++){
+//                Card card = drawPile.drawCard();
+//                player.takeCard(card);
+//                turnManager.nextPlayer();
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Adds players to the game based on number of players selected in SetupView
+//     * TODO: Refactor this - have another class to manage/initialize players?
+//     */
+//    private void addPlayers(){
+//        for (int i = 0; i < settings.getNumPlayers(); i++){
+//            turnManager.addPlayer(new Player());
+//        }
+//    }
+//
+////    /**
+////     * Handle effect of an action card when it played
+////     * This method should have access to TurnManager, especially for Reverse cards
+////     * TODO: put action methods in a resource file and use this method to call them
+////     */
+////    public void handleAction(){
+////
+////    }
+////
+////    /**
+////     * Allows the human player to declare uno
+////     * Called from view class when the "uno" button is clicked
+////     */
+////    public void callUno(){
+////
+////    }
+////
+////    /**
+////     * Once a player has no cards left, check if they have declared uno
+////     * If a player has not declared, then they must pick up more cards
+////     */
+////    public void checkUno(){
+////
+////    }
+//
+//}
--- test/ooga/piles/DeckTest.java
+++ test/ooga/piles/DeckTest.java
@@ -104,24 +104,66 @@ class DeckTest {
         assertEquals(1, deck6.getAllCards().size());
     }
 
+    /**
+     * Tests adding a List of Cards with the addCards() method when
+     * there are multiple Cards in the List.
+     */
     @Test
     void addCardsMultiple() {
         Deck deck7 = new DiscardPile();
         Card card1 = new Card(Suit.D, Value.NINE);
         Card card2 = new Card(Suit.A, Value.ONE);
         Card cardiB = new Card(Suit.D, Value.ONE);
-       //TODO: finish writing this test
+
         List<Card> moreCards = new ArrayList<>(Arrays.asList(card1, card2, cardiB));
         deck7.addCards(moreCards);
-        assertEquals(card1, deck7.getAllCards().peek());
-        assertEquals(1, deck7.getAllCards().size());
+
+        assertEquals(3, deck7.getAllCards().size());
+        assertEquals(cardiB, deck7.getAllCards().pop());
+        assertEquals(card2, deck7.getAllCards().pop());
+        assertEquals(card1, deck7.getAllCards().pop());
     }
 
+    /**
+     * Tests the getAllCards() method with an empty Deck.
+     */
     @Test
-    void getAllCards() {
+    void getAllCardsEmptyDeck() {
+        Deck deck8 = new DiscardPile();
+        assertEquals(new Stack<Card>(), deck8.getAllCards());
     }
 
+    /**
+     * Tests the getAllCards() method with a populated Deck.
+     */
     @Test
-    void getCardCount() {
+    void getAllCardsNormalDeck() {
+        Deck deck9 = new DiscardPile();
+        Card card1 = new Card(Suit.D, Value.NINE);
+        Card card2 = new Card(Suit.A, Value.ONE);
+        deck9.addCards(Arrays.asList(card1, card2));
+
+        Stack<Card> expected = new Stack<>();
+        expected.push(card1);
+        expected.push(card2);
+        assertEquals(expected, deck9.getAllCards());
+    }
+
+    /**
+     * Tests getCardCount() for a populated Deck.
+     */
+    @Test
+    void getCardCountFullDeck() {
+        Deck deck10 = new DrawPile();
+        assertEquals(80, deck10.getCardCount());
+    }
+
+    /**
+     * Tests getCardCount() for an empty Deck.
+     */
+    @Test
+    void getCardCountEmptyDeck() {
+        Deck deck11 = new DiscardPile();
+        assertEquals(0, deck11.getCardCount());
     }
 }

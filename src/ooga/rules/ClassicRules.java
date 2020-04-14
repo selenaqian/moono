66839cbf947332be6f0f43aae8bc7e8f8cded717
Tess Noonan <tcn6@duke.edu>
2020-04-14 13:17:33 -0600

    updated ClassicRules for wild cards

--- src/ooga/rules/ClassicRules.java
+++ src/ooga/rules/ClassicRules.java
@@ -1,11 +1,12 @@
 package ooga.rules;
 
 import ooga.cards.Card;
+import ooga.cards.Value;
 import ooga.piles.Hand;
 
 /**
  * This class follows the classic uno rules: (WHICH IS ONLY USING BASIC CARDS FOR SPRINT 1)
- *      Must play a card that is either of same Suit or Value as the Card on top of DiscardPile.
+ *      Must play a card that is either of same Suit or Value as the Card on top of DiscardPile (unless it's Wild).
  *      Game is over when the last card in a player's hand is valid.
  */
 public class ClassicRules implements Rule {
@@ -18,7 +19,10 @@ public class ClassicRules implements Rule {
 
     @Override
     public boolean isValid(Card discard, Card playedCard) {
-        if(playedCard.getSuit() == discard.getSuit() || playedCard.getValue() == discard.getValue()){
+        if(playedCard.getValue() == Value.WILD || playedCard.getValue() == Value.WILD4){
+            return true;
+        }
+        else if(playedCard.getSuit() == discard.getSuit() || playedCard.getValue() == discard.getValue()){
             return true;
         }
         return false;

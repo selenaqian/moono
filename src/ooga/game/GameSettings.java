041ca070c00527e7eb3713b198eda504f2a63bcc
Mary Jiang <mvj6@duke.edu>
2020-04-15 19:55:01 -0600

    merged master

--- src/ooga/game/GameSettings.java
+++ src/ooga/game/GameSettings.java
@@ -1,5 +1,11 @@
 package ooga.game;
 
+import ooga.cards.Card;
+import ooga.cards.Value;
+import ooga.rules.Rule;
+
+import java.util.List;
+
 /**
  * Functions as a controller class
  * Called in SetupView
@@ -9,6 +15,8 @@ public class GameSettings {
     private int numPlayers; //set to default 4 players
     private int handSize; //number of cards per player, set to default 7
     private int winningScore; //default 500 for uno
+    private List<Card> specialCards;
+    private Rule rule;
 
     public GameSettings(){
         numPlayers = 4;
@@ -38,6 +46,23 @@ public class GameSettings {
 
     public void setWinningScore(int winningScore){
         this.winningScore = winningScore;
+    }
+
+    public void setSpecialCards(List<String> specialCardValues){
+        //TODO get values enum from string
+
+    }
+
+    public void setRules(String rule){
+        //TODO: implement java reflection
+    }
+
+    public Rule getRule(){
+        return rule;
+    }
+
+    public List<Card> getSpecialCards(){
+        return specialCards;
     }
 
     //TODO: add setters and getters for: Rules, Themes, Special cards

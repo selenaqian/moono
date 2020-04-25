e5d747ad8a4fe165266428296b581c5103e38a59
Mary Jiang <mvj6@duke.edu>
2020-04-24 22:24:14 -0600

    fixes for UnoScoreTracker

--- test/ooga/game/UnoScoreTrackerTest.java
+++ test/ooga/game/UnoScoreTrackerTest.java
@@ -8,22 +8,40 @@ import ooga.player.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UnoScoreTrackerTest {
 
     Uno uno = new Uno();
     GameSettings settings = new GameSettings();
-    UnoScoreTracker scoreTracker = new UnoScoreTracker();
+    UnoScoreTracker scoreTracker;
+    ArrayList<Player> players;
 
-    AI_Player player1 = new AI_Player();
-    AI_Player player2 = new AI_Player();
-    AI_Player player3 = new AI_Player();
+    AI_Player player1;
+    AI_Player player2;
+    AI_Player player3;
 
 
 
     @BeforeEach
-    void setUpPlayerHands(){
+    void setUpPlayers(){
+        scoreTracker = new UnoScoreTracker();
+        players = new ArrayList<>();
+        player1 = new AI_Player();
+        player2 = new AI_Player();
+        player3 = new AI_Player();
+
+        player1.setID(1);
+        player2.setID(2);
+        player3.setID(3);
+
+        players.add(player1);
+        players.add(player2);
+        players.add(player3);
+
+
         player1.takecard(new Card(Suit.A, Value.ONE));
         player1.takecard(new Card(Suit.A, Value.TWO));
         player1.takecard(new Card(Suit.A, Value.REVERSE));
@@ -34,13 +52,20 @@ class UnoScoreTrackerTest {
 
     @Test
     void calcPlayerScore() {
+        scoreTracker.calculate(players);
         assertEquals(73, scoreTracker.getPlayerScore(player1));
         assertEquals(50, scoreTracker.getPlayerScore(player2));
-
+        assertEquals(0, scoreTracker.getPlayerScore(player3));
     }
 
     @Test
-    void testPlayerHasNoScore(){
+    void calcAddedUpdatedScore(){
+        scoreTracker.calculate(players);
+        scoreTracker.calculate(players);
+        assertEquals(146, scoreTracker.getPlayerScore(player1));
+        assertEquals(100, scoreTracker.getPlayerScore(player2));
         assertEquals(0, scoreTracker.getPlayerScore(player3));
+
     }
+
 }

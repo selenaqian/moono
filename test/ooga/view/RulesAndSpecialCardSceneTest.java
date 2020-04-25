4b27274f18a3769365910591ad1506fab6ce71e6
Selena Qian <sq22@duke.edu>
2020-04-24 20:23:29 -0400

    wrote outlines of tests to be written

--- test/ooga/view/RulesAndSpecialCardSceneTest.java
+++ test/ooga/view/RulesAndSpecialCardSceneTest.java
@@ -1,7 +1,52 @@
 package ooga.view;
 
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import util.DukeApplicationTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class RulesAndSpecialCardSceneTest {
+class RulesAndSpecialCardSceneTest extends DukeApplicationTest {
+    private SetupView startView;
 
+    @Override
+    public void start(Stage stage) {
+        startView = new SetupView(stage);
+        clickOn(startView.getWelcomeOkButton()); // advances to the rules and special cards selection scene
+    }
+
+    @Test
+    void defaultRulesSelectionsTest() {
+
+    }
+
+    @Test
+    void deadlyRulesTest() {
+
+    }
+
+    @Test
+    void ascendingRulesTest() {
+
+    }
+
+    @Test
+    void noSpecialCardsTest() {
+
+    }
+
+    @Test
+    void oneSpecialCardTest() {
+
+    }
+
+    @Test
+    void multipleSpecialCardsTest() {
+
+    }
+
+    @Test
+    void allSpecialCardsTest() {
+
+    }
 }
--- test/ooga/view/WildColorSelectorViewTest.java
+++ test/ooga/view/WildColorSelectorViewTest.java
@@ -1,14 +1,31 @@
 package ooga.view;
 
+import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import util.DukeApplicationTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class WildColorSelectorViewTest extends DukeApplicationTest {
+    private WildColorSelectorView wildColorSelector;
+
+    @Override
+    public void start(Stage stage) {
+
+    }
 
     @Test
     void showColorSelectorTestElements() {
         //test to make sure correctly generating the visuals here
+    }
+
+    @Test
+    void colorSelectedTest1() {
+        //check that color has been set? is it possible to access this from here?
+    }
+
+    @Test
+    void colorSelectedTest2() {
+
     }
 }

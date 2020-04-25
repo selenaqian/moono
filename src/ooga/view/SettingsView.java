3dbfca865f9a71bf8df02b3e396dee5b2d3706fb
Selena Qian <sq22@duke.edu>
2020-04-25 01:04:41 -0400

    finished writing tests and debugging mid-game theme changes

--- src/ooga/view/SettingsView.java
+++ src/ooga/view/SettingsView.java
@@ -14,6 +14,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import ooga.game.GameSettings;
+import ooga.game.UnoController;
 
 import java.util.*;
 
@@ -51,7 +52,10 @@ public class SettingsView {
         themeOptions = new HashMap<>();
         themeFileNames = new HashMap<>();
         saveNew = new MidGameSaveNew(myGameView.getStage(), myStage, myGameView.getController());
+        showSettingsView();
+    }
 
+    private void showSettingsView() {
         //TODO: check if this works
         myStage.setOnCloseRequest(e -> myGameView.getController().play());
 
@@ -80,16 +84,18 @@ public class SettingsView {
         speedButtons.setAlignment(Pos.CENTER);
         speedChangeText = new Text(mySettings.getSpeed() + myResources.getString("speedSuffix")); //TODO: change to display # of seconds?
         slowDownButton = new Button(myResources.getString("slowDownButton"));
-        slowDownButton.setOnMouseClicked(e -> {
+        slowDownButton.setOnAction(e -> {
             double newSpeed = mySettings.getSpeed() + 0.5;
             myGameView.getController().changeSpeed(newSpeed);
+            mySettings.setSpeed(newSpeed);
             speedChangeText.setText(newSpeed + myResources.getString("speedSuffix"));
         });
         speedUpButton = new Button(myResources.getString("speedUpButton"));
-        speedUpButton.setOnMouseClicked(e -> {
+        speedUpButton.setOnAction(e -> {
             if(mySettings.getSpeed() > 1) {
                 double newSpeed = mySettings.getSpeed() - 0.5;
                 myGameView.getController().changeSpeed(newSpeed);
+                mySettings.setSpeed(newSpeed);
                 speedChangeText.setText(newSpeed + myResources.getString("speedSuffix"));
             }
         });
@@ -111,24 +117,32 @@ public class SettingsView {
         String[] allThemeFiles = myResources.getString("themeFileNames").split(",");
         for(int i=0; i < allThemes.length; i++) {
             String theme = allThemes[i];
-            Pane currentPane = new StackPane();
-            int sceneWidth = DEFAULT_STAGE_WIDTH/2;
-            int rectSize = sceneWidth/(allThemes.length + 1);
-            Color currentColor = Color.valueOf(myResources.getString(theme).split(",")[0]);
-            Rectangle colorBox = new Rectangle(rectSize, rectSize, currentColor);
-            colorBox.setOnMouseClicked(e -> themeChosen(theme));
-            Text colorText = new Text(theme);
-            colorText.getStyleClass().add("settingsThemeText");
-            colorText.setOnMouseClicked(e -> themeChosen(theme));
-
-            currentPane.getChildren().addAll(colorBox, colorText);
+            Pane currentPane = makeThemeOption(allThemes, theme);
             themeOptionsToClick.getChildren().add(currentPane);
             themeOptions.put(theme, currentPane);
             themeFileNames.put(theme, allThemeFiles[i]);
         }
+
         darkModeToggle = new CheckBox(myResources.getString("darkMode"));
         themeUI.getChildren().addAll(themeTitle, themeInstructions, darkModeToggle, themeOptionsToClick);
         return themeUI;
+    }
+
+    private Pane makeThemeOption(String[] allThemes, String theme) {
+        Pane currentPane = new StackPane();
+        int sceneWidth = DEFAULT_STAGE_WIDTH/2;
+        int rectSize = sceneWidth/(allThemes.length + 1);
+        Color currentColor = Color.valueOf(myResources.getString(theme).split(",")[0]);
+
+        Rectangle colorBox = new Rectangle(rectSize, rectSize, currentColor);
+        colorBox.setOnMouseClicked(e -> themeChosen(theme));
+
+        Text colorText = new Text(theme);
+        colorText.getStyleClass().add("settingsThemeText");
+        colorText.setOnMouseClicked(e -> themeChosen(theme));
+
+        currentPane.getChildren().addAll(colorBox, colorText);
+        return currentPane;
     }
 
     private HBox makeSaveAndNewGameUI() {
@@ -152,12 +166,39 @@ public class SettingsView {
         colorBox.getStyleClass().add("themeOptionSelected");
         if(!darkModeToggle.isSelected()) {
             myGameView.setTheme(themeFileNames.get(theme));
+            mySettings.setTheme(themeFileNames.get(theme));
         }
         else {
             String themeFileName = themeFileNames.get(theme);
             themeFileName = themeFileName.substring(0, themeFileName.length()-4);
             myGameView.setTheme(themeFileName + "_darkMode.css");
+            mySettings.setTheme(themeFileName + "_darkMode.css");
         }
-        //TODO: make the css files
+    }
+
+    //Methods below used for testing.
+
+    /**
+     * Allows test to access the controller and verify that the speed has changed appropriately.
+     * @return the UnoController associated with this SettingsView and the game as a whole.
+     */
+    public UnoController getController() {
+        return myGameView.getController();
+    }
+
+    /**
+     * Allows test to access the game settings and verify that speed has changed appropriately.
+     * @return the GameSettings associated with this SettingsView and the game as a whole.
+     */
+    public GameSettings getSettings() {
+        return mySettings;
+    }
+
+    /**
+     * Allows test to access the theme options and check that clicking on those properly affects the GameView.
+     * @return the map of theme option names to their panes in the scene.
+     */
+    public Map<String, Pane> getThemeOptions() {
+        return themeOptions;
     }
 }
--- test/ooga/view/SettingsViewTest.java
+++ test/ooga/view/SettingsViewTest.java
@@ -1,47 +1,83 @@
 package ooga.view;
 
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import util.DukeApplicationTest;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SettingsViewTest extends DukeApplicationTest {
     private SettingsView mySettingsView;
+    private Button speedUp;
+    private Button slowDown;
+    private Map<String, Pane> themeOptions;
 
     @Override
     public void start(Stage stage) {
-
+        mySettingsView = new SettingsView();
+        speedUp = lookup("speed up").queryButton();
+        slowDown = lookup("slow down").queryButton();
+        themeOptions = mySettingsView.getThemeOptions();
     }
 
     @Test
     void speedUpTest1() {
-
+        javafxRun(() -> clickOn(speedUp));
+        sleep(2, TimeUnit.SECONDS);
+        assertNotNull(lookup("1.0 s").query());
     }
 
     @Test
     void speedUpTest2() {
+        javafxRun(() -> clickOn(speedUp));
+        javafxRun(() -> clickOn(speedUp));
 
+        assertNotNull(lookup("1.0 s").query());
+        assertEquals(1.0, mySettingsView.getSettings().getSpeed());
     }
 
     @Test
     void slowDownTest1() {
-
+        javafxRun(() -> clickOn(slowDown));
+        assertNotNull(lookup("2.0 s").query());
+        assertEquals(2.0, mySettingsView.getSettings().getSpeed());
     }
 
     @Test
     void slowDownTest2() {
+        javafxRun(() -> clickOn(slowDown));
+        javafxRun(() -> clickOn(slowDown));
 
+        assertNotNull(lookup("2.5 s").query());
+        assertEquals(2.5, mySettingsView.getSettings().getSpeed());
     }
 
     @Test
     void themeChangeDukeTest() {
-
+        Rectangle dukeOption = (Rectangle)themeOptions.get("duke").getChildren().get(0);
+        javafxRun(() -> clickOn(dukeOption));
+        sleep(2, TimeUnit.SECONDS);
+        assertEquals("themeOptionSelected", dukeOption.getStyleClass().get(0));
+        assertEquals("stylesheets/duke.css", mySettingsView.getSettings().getTheme());
     }
 
     @Test
     void themeChangeSpaceTest() {
-
+        Rectangle dukeOption = (Rectangle)themeOptions.get("duke").getChildren().get(0);
+        javafxRun(() -> clickOn(dukeOption));
+        Rectangle spaceOption = (Rectangle)themeOptions.get("space").getChildren().get(0);
+        javafxRun(() -> clickOn(spaceOption));
+        assertEquals("themeOptionSelected", spaceOption.getStyleClass().get(0));
+        assertEquals("stylesheets/space.css", mySettingsView.getSettings().getTheme());
+        assertEquals(0, dukeOption.getStyleClass().size());
     }
 
     @Test
